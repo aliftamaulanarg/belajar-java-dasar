046f4df48d69eef6859044f1732b674a60880ae3
aliftamaulanarg <aliftamaulanarg@gmail.com>
2024-02-07 11:41:08 +0700

    menambahkan file Komentar.java

---
+++ belajar-java-dasar/src/Komentar.java
@@ -0,0 +1,18 @@
+public class Komentar {
+
+    public static void main (String[] args){
+
+        // ini adalah test catatan / komentar, dengan menggunakan garis miring ini
+        // maka baris kode ini tidak akan di jalankan oleh program.
+
+        // test satu, test 2
+        // test 3, test 4
+        // test 5
+
+        int angka;
+        angka = 10 + 10;
+
+        System.out.println(angka);
+        
+    }
+}
