1590c6b1da9bcba55b38e58c81d3721446e242af
aliftamaulanarg <aliftamaulanarg@gmail.com>
2024-02-07 11:21:29 +0700

    test pertama

---
+++ belajar-java-dasar/src/TipeData.java
@@ -0,0 +1,27 @@
+public class TipeData {
+
+    public static void main(String[] args) {
+
+        /*String namaLengkap = "rayhan";
+        int umur = 23;
+
+        System.out.println(namaLengkap + " " + umur);
+        System.out.println(namaLengkap + " " + umur);
+
+        System.out.println("Points:"); */
+
+        /*int x = 15; int y = 4;
+        int result = x / y;
+
+        System.out.println(result);*/
+
+        int egg = 54;
+        int bag = 8;
+
+        int result = egg % bag;
+
+        System.out.println(result);
+    }
+}
+
+
