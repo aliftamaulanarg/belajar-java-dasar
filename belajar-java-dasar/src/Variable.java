2fe8035a0362f2e41072ab339f058667a80963c7
aliftamaulanarg <aliftamaulanarg@gmail.com>
2024-02-07 11:57:28 +0700

    menambahkan file variable ke dalam project

---
+++ belajar-java-dasar/src/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+
+    public static void main(String[] args){
+
+        String namaLengkap;
+        namaLengkap = "Alifta Maulana Rayhan Giraldi";
+
+        System.out.println(namaLengkap);
+
+        
+        int umur = 30;
+
+        System.out.println("umur saya saat ini adalah : " + umur);
+    }
+}
